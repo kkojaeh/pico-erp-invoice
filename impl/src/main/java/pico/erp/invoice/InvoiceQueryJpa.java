edf7dfc45473ab148db70fdec006e6150dd467d5
Ethan <kkojaeh@gmail.com>
2019-01-09 09:17:49 +0900

    query 빈 등록

--- impl/src/main/java/pico/erp/invoice/InvoiceQueryJpa.java
+++ impl/src/main/java/pico/erp/invoice/InvoiceQueryJpa.java
@@ -12,10 +12,18 @@ import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.annotation.Validated;
 import pico.erp.invoice.InvoiceView.Filter;
 import pico.erp.invoice.item.QInvoiceItemEntity;
+import pico.erp.shared.Public;
 import pico.erp.shared.jpa.QueryDslJpaSupport;
 
+@Service
+@Public
+@Transactional(readOnly = true)
+@Validated
 public class InvoiceQueryJpa implements InvoiceQuery {
 
 
