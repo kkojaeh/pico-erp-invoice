21b531f9efb53a01a38c9afb08ca8d1f195be7ae
Ethan <kkojaeh@gmail.com>
2019-03-07 13:33:35 +0900

    프로필 설정 변경

--- test/src/main/java/pico/erp/invoice/TestDataInitializer.java
+++ test/src/main/java/pico/erp/invoice/TestDataInitializer.java
@@ -12,7 +12,7 @@ import pico.erp.shared.ApplicationInitializer;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Configuration
-@Profile({"!development", "!production"})
+@Profile({"test-data"})
 public class TestDataInitializer implements ApplicationInitializer {
 
   @Lazy
